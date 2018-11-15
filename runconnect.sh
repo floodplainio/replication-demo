7780a65f64c4a1ed436555972f66b5f6366ae79c
Frank Lyaruu <frank@dexels.com>
2018-11-15 09:31:16 +0100

    updates

--- runconnect.sh
+++ runconnect.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run --network=debezium_default -p 8083:8083 -e BOOTSTRAP_SERVERS=kafka:9092 -e CONFIG_STORAGE_TOPIC=topic-config -e OFFSET_STORAGE_TOPIC=offset-storage debezium/connect:0.8
+docker run --network=replication-demo_default -p 8083:8083 -e BOOTSTRAP_SERVERS=kafka:9092 -e CONFIG_STORAGE_TOPIC=topic-config -e OFFSET_STORAGE_TOPIC=offset-storage debezium/connect:0.9
--- runelastic.sh
+++ runelastic.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-docker run -d --network=debezium_default -e discovery.type=single-node -p 9200:9200 -p 9300:9300 docker.elastic.co/elasticsearch/elasticsearch-oss:6.3.1
+docker run -d --network=debezium_default -e discovery.type=single-node -p 9200:9200 -p 9300:9300 docker.elastic.co/elasticsearch/elasticsearch-oss:6.4.2
 #  elasticsearch:
 #    image: docker.elastic.co/elasticsearch/elasticsearch-oss:6.3.1
 #    environment:
