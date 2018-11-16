83fc4195d19626b5c83d41f779e09781b9525067
Frank Lyaruu <frank@dexels.com>
2018-11-16 13:32:54 +0100

    clean up data

--- mockup/postgres/connect.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-docker run -d --rm --name connect -p 8083:8083 -e BOOTSTRAP_SERVERS="10.0.0.7:9092" -e GROUP_ID=1 -e CONFIG_STORAGE_TOPIC=my_connect_configs -e OFFSET_STORAGE_TOPIC=my_connect_offsets debezium/connect:0.7
--- mockup/postgres/post.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-curl -i -X POST -H "Accept:application/json" -H "Content-Type:application/json" -d @connect.json localhost:8083/connectors/
-
