239c780183c34ac1a24d628dc01f080271c41471
Frank Lyaruu <frank@dexels.com>
2019-09-17 12:29:42 +0200

    update and clean

--- mockup/create.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-export PGPASSWORD="mysecretpassword"
-psql -U postgres -h localhost -c "CREATE DATABASE dvdrental;"
-
--- mockup/restore.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-export PGPASSWORD="mysecretpassword"
-pg_restore -h localhost  -U postgres -d dvdrental dvdrental.tar 
-
